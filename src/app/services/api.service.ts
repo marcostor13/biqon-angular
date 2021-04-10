b45f0aee4dd0cb2df37eabc5924351657a451ff4
Marcos Torres <marcostor13@gmail.com>
2021-04-09 20:14:32 -0500

    Htacces y Cambio de servidor backend

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -12,7 +12,7 @@ export class ApiService {
 
   constructor(private http: HttpClient, private cookie: CookieService, private router: Router) {
     if (window.location.href.indexOf('35.238.14.128') > -1 || window.location.href.indexOf('binteraction' ) > -1) {
-      this.url = 'https://contactos.binteraction.cl/api/';
+      this.url = 'https://apicontactos.binteraction.cl/api/';
     } else {
       this.url = 'http://localhost:8000/api/';
     }
